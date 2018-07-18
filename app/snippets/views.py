3c425330cf1e988a97ad547c0ca7979c9cfbd077
sangmin <sang93423@gmail.com>
2018-07-18 13:08:34 +0900

    view에다가 get요청, post요청 보내는 코드 추가 snippet serializer기능 사용

--- app/snippets/views.py
+++ app/snippets/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 
-# Create your views here.
+from snippets.serializers import SnippetSerializer
+from .models import Snippet
+
+
+
+class JsonResponse(HttpResponse):
+	def __init__(self, data, **kwargs):
+		content = JSONRenderer().render(data)
+		kwargs['content_type'] = 'application/json'
+		super().__init__(content, **kwargs)
+
+@csrf_exempt
+def snippet_list(request):
+	if request.method =='GET':
+		snippets = Snippet.objects.all()
+		serializer = SnippetSerializer(snippets, many=True)
+		json_data = JSONRenderer().render(serializer.data)
+		return HttpResponse(json_data, content_type='application/json')
+	elif request.method == 'POST':
+		data = JSONParser().parse(request)
+		serializer = SnippetSerializer(data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data, status=201)
+		return JsonResponse(serializer.errors, status=400)
+
