dd97fe01ce7c4743d40d6c1f648441d4132a0134
sangmin <sang93423@gmail.com>
2018-07-23 16:03:30 +0900

    APIView사용

---
+++ app/snippets/views/__init__.py
@@ -0,0 +1,9 @@
+# from django.urls import path, include
+# #
+# # from . import django_view, api_view
+# #
+# # app_name = 'snippets'
+# # urlpatterns = [
+# # 	path('django_view/', include(django_view)),
+# # 	path('api_view/', include(api_view)),
+# # ]
